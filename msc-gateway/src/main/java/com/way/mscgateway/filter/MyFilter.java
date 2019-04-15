8c908071d4a547a96b5cd042634ab95287e8ba4a
wayleung <610686949@qq.com>
2019-04-16 01:09:17 +0800

    add filter by zuul

---
+++ msc-gateway/src/main/java/com/way/mscgateway/filter/MyFilter.java
@@ -0,0 +1,49 @@
+package com.way.mscgateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+@Slf4j
+public class MyFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        String token = request.getParameter("token");
+        if(token ==null){
+            log.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            }catch (Exception e){
+
+            }
+            return null;
+        }
+        log.info("ok");
+        return null;
+    }
+}
