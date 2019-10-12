712e47248f2741a75f218e26fb210337f7c574c6
liangliwei <liangliwei@bluemoon.com.cn>
2019-10-12 10:35:04 +0800

    add service consumer msc-teacher to consume student service by ribbon

--- msc-teacher/src/main/java/com/way/mscteacher/MscTeacherApplication.java
+++ msc-teacher/src/main/java/com/way/mscteacher/MscTeacherApplication.java
@@ -2,12 +2,26 @@ package com.way.mscteacher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+//druid 是一个servlet 需要让SpingBoot支持servlet 且访问地址是 http://127.0.0.1:8762/druid/ 不是localhost
+@ServletComponentScan
 public class MscTeacherApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MscTeacherApplication.class, args);
     }
 
+    @Bean
+    @LoadBalanced
+//    @LoadBalanced注解表明这个restRemplate开启负载均衡的功能。
+    RestTemplate getRestemplate(){
+        return new RestTemplate();
+    }
 }
--- msc-teacher/src/main/java/com/way/mscteacher/controller/TeacherController.java
+++ msc-teacher/src/main/java/com/way/mscteacher/controller/TeacherController.java
@@ -1,11 +1,13 @@
 package com.way.mscteacher.controller;
 
 import com.way.mscteacher.config.ServerConfig;
+import com.way.mscteacher.dao.entity.Student;
 import com.way.mscteacher.dao.entity.Teacher;
 import com.way.mscteacher.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.net.UnknownHostException;
 import java.util.List;
@@ -22,6 +24,9 @@ public class TeacherController {
     @Autowired
     ServerConfig serverConfig;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @GetMapping("/teacher")
     public List<Teacher> getAllTeacher(){
         return teacherService.getAllTeachers();
@@ -36,4 +41,15 @@ public class TeacherController {
     public String hi() throws UnknownHostException {
         return "hi,i am from :"+serverConfig.getUrl();
     }
+
+    @GetMapping("/teacher/hi")
+    public String getHiFromStudent(){
+        return restTemplate.getForObject("http://MSC-STUDENT/hi",String.class);
+    }
+
+    @GetMapping("/teacher/student")
+    public List<Student> getAllStudentFromStudent(){
+        return restTemplate.getForObject("http://MSC-STUDENT/student",List.class);
+    }
+
 }
