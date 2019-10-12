c2e4732b2cb68dee8cdf8110d24501a127d8a955
liangliwei <liangliwei@bluemoon.com.cn>
2019-10-12 16:31:43 +0800

    build msc-zuul service for routing

---
+++ msc-zuul/src/main/java/com/way/msczuul/MscZuulApplication.java
@@ -0,0 +1,17 @@
+package com.way.msczuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class MscZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MscZuulApplication.class, args);
+    }
+
+}
